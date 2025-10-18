707a469dbafbece40ef7ebafbf8690c2a44c1157
prajwal1357 <prajwalvr1357@gmail.com>
2025-10-18 12:20:57 +0530

    trip form done

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,14 +21,14 @@ const Header = ({ title, description, ctaText, ctaUrl }: Props) => {
                 <p className={cn("text-gray-100 font-normal",  location.pathname === '/' ? 'text-base md:text-lg': 'text-sm md:text-lg')}>{description}</p>
             </article>
 
-            {/* {ctaText && ctaUrl && (
+            {ctaText && ctaUrl && (
                 <Link to={ctaUrl}>
                     <ButtonComponent type="button" className="button-class !h-11 !w-full md:w-[240px]">
                         <img src="/assets/icons/plus.svg" alt="plus" className="size-5" />
                         <span className="p-16-semibold text-white">{ctaText}</span>
                     </ButtonComponent>
                 </Link>
-            )} */}
+            )}
         </header>
     )
 }
